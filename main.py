6e36c2430ad934faa69b83af9d6e0a1886d32fe7
annakertesz <kerteszannanak@gmail.com>
2016-07-19 14:48:43 +0200

    Half working menu

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from models import *
+from menu_system import menu
 
-# Write here your console application
+
+main_menu = ("Application's menu", "Administrator's menu")
+
+print (" --- WELCOME TO CODECOOL APPLICATION SYSTEM ---")
+
+while True:
+    user_input = menu(main_menu)
+
+
+    if user_input == 1:
+
+        print ("app menu")
+
+    if user_input == 2:
+
+        print ("adm menu")
+
+    print (menu(main_menu))
