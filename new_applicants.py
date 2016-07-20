ddf617718d29bce67445e54d32204c4cb7e72ebd
handriss <andrashinkel@gmail.com>
2016-07-20 13:08:26 +0200

    city table implemented

--- new_applicants.py
+++ new_applicants.py
@@ -8,96 +8,47 @@ db.create_tables([School, Applicant, City, Mentor], safe=True)
 
 bp = School.create(city="Budapest")
 miskolc = School.create(city="Miskolc")
+krakow = School.create(city="Krakow")
 
-applicant1 = Applicant.create(
-    first_name="Jack",
-    last_name="Johnson",
-    email="jackjohnson@gmail.com",
-    city="Érd",
-    application_code=None,
-    school=None
-)
+cities = {
+    "Érd": "Budapest",
+    "Kiskunbüdösbütykös": "Miskolc",
+    "Mars": "Miskolc",
+    "Tijuana": "Budapest",
+    "Decapod 10": "Miskolc",
+    "Nimbus": "Budapest",
+    "New New York": "Budapest"
+}
 
-applicant2 = Applicant.create(
-    first_name="John",
-    last_name="Jackson",
-    email="johnjackson@gmail.com",
-    city="Kiskunbüdösbütykös",
-    application_code=None,
-    school=None
-)
+applicants = [
+    {"first_name": "Jack", "last_name": "Johnson", "email": "jackjohnson@gmail.com", "city": "Érd"},
+    {"first_name": "John", "last_name": "Jackson", "email": "johnjackson@gmail.com", "city": "Kiskunbüdösbütykös"},
+    {"first_name": "Amy", "last_name": "Wong", "email": "awong79@marslink.web", "city": "Mars"},
+    {"first_name": "Bender", "last_name": "Bending Rodriguez", "email": "bender@ilovebender.com", "city": "Tijuana"},
+    {"first_name": "John", "last_name": "Zoidberg", "email": "zoidberg@decapodians.deca", "city": "Decapod 10"},
+    {"first_name": "Kif", "last_name": "Kroker", "email": "iamgreen@military.earth", "city": "Nimbus"},
+    {"first_name": "Zapp", "last_name": "Brannigan", "email": "lovethezapper@military.earth", "city": "Nimbus"},
+    {"first_name": "Hermes", "last_name": "Conrad", "email": "bureaucrat_conrad@gmail.com", "city": "New New York"},
+    {"first_name": "Antonio", "last_name": "Calculon", "email": "allmycircuits@gmail.com", "city": "New New York"},
+    {"first_name": "Philip", "last_name": "Fry", "email": "iloveleela@gmail.com", "city": "New New York"}
+]
 
-applicant3 = Applicant.create(
-    first_name="Amy",
-    last_name="Wong",
-    email="awong79@marslink.web",
-    city="Mars",
-    application_code=None,
-    school=None
-)
-
-applicant4 = Applicant.create(
-    first_name="Bender",
-    last_name="Bending Rodriguez",
-    email="bender@ilovebender.com",
-    city="Tijuana",
-    application_code=None,
-    school=None
-)
-
-applicant5 = Applicant.create(
-    first_name="John",
-    last_name="Zoidberg",
-    email="zoidberg@decapodians.deca",
-    city="Decapod 10",
-    application_code=None,
-    school=None
-)
-
-applicant6 = Applicant.create(
-    first_name="Kif",
-    last_name="Kroker",
-    email="iamgreen@military.earth",
-    city="Nimbus",
-    application_code=None,
-    school=None
-)
-
-applicant7 = Applicant.create(
-    first_name="Zapp",
-    last_name="Brannigan",
-    email="lovethezapper@military.earth",
-    city="Nimbus",
-    application_code=None,
-    school=None
-)
-
-applicant8 = Applicant.create(
-    first_name="John",
-    last_name="Zoidberg",
-    email="zoidberg@decapodians.deca",
-    city="Decapod 10",
-    application_code=None,
-    school=None
-)
-
-applicant9 = Applicant.create(
-    first_name="Antonio",
-    last_name="Calculon",
-    email="allmycircuits@gmail.com",
-    city="New New York",
-    application_code=None,
-    school=None
-)
-
-applicant10 = Applicant.create(
-    first_name="Philip",
-    last_name="Fry",
-    email="iloveleela@gmail.com",
-    city="New New York",
-    application_code=None,
-    school=None
-)
+for school in schools:
+    School.create(
+        city=school
+    )
+for key, value in cities.items():
+    City.create(
+        city=key,
+        school_city=value
+    )
+for applicant in applicants:
+    Applicant.create(
+        first_name=applicant.get("first_name"),
+        last_name=applicant.get("last_name"),
+        email=applicant.get("email"),
+        city=applicant.get("city"),
+    )
 
 mentor1 = Mentor.create(
     first_name="Lord",
@@ -112,5 +63,3 @@ mentor2 = Mentor.create(
     email="",
     school=miskolc
 )
-
-# print(bp1.applicants()[1].first_name)
