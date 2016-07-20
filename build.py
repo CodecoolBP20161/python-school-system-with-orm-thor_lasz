a4aad85936dddb8ebfb4728f5941fce1683ade6c
handriss <andrashinkel@gmail.com>
2016-07-20 20:44:56 +0200

    Housekeeping done, unnecessary modules deleted

--- build.py
+++
@@ -1,7 +0,0 @@
-# This script can create the database tables based on your models
-
-from models import *
-
-db.connect()
-# List the tables here what you want to create...
-db.create_tables([], safe=True)
--- example_data.py
+++
@@ -1,3 +0,0 @@
-# This script can generate example data for "City" and "InterviewSlot" models.
-
-from models import *
