52752b8c4558a6775c13a410d46259f30f454918
handriss <andrashinkel@gmail.com>
2016-07-28 10:16:15 +0200

    Entry box table and fetch button implemented

--- table_gui.py
+++ table_gui.py
@@ -7,65 +7,47 @@ class TableWindow():
     def __init__(self, master):
         self.master = master
         n = ttk.Notebook(self.master)
-        f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
-        f2 = ttk.Frame(n)   # second page
-        f3 = ttk.Frame(n)   # second page
-        f4 = ttk.Frame(n)   # second page
-        f5 = ttk.Frame(n)   # second page
-        n.add(f1, text='Applicant')
-        n.add(f2, text='City')
-        n.add(f3, text='Mentor')
-        n.add(f4, text='InterviewSlot')
-        n.add(f5, text='School')
+        table_names = ["Applicant", "City", "Mentor", "InterviewSlot", "School"]
+        tables = []
 
+        for table in table_names:
+            tables.append(ttk.Frame(n))
+            n.add(tables[-1], text=table)
         n.pack()
-        Populator.establish_connection()
-        Populator.populate_tables()
 
         table = Applicant.select().tuples()
-        self.table_filler(table, f1, ["ID", "First name", "Last name", "Email", "City", "Application code", "School", "Interview", "Status"])
+        self.table_filler(table, tables[0], Applicant._meta.sorted_field_names)
         table = City.select().tuples()
-        self.table_filler(table, f2, ["ID", "City", "School city"])
+        self.table_filler(table, tables[1], City._meta.sorted_field_names)
         table = Mentor.select().tuples()
-        self.table_filler(table, f3, ["ID", "First name", "Last name", "Email", "School"])
+        self.table_filler(table, tables[2], Mentor._meta.sorted_field_names)
         table = InterviewSlot.select().tuples()
-        self.table_filler(table, f4, ["ID", "Start", "End", "Reserved", "Mentor"])
+        self.table_filler(table, tables[3], InterviewSlot._meta.sorted_field_names)
         table = School.select().tuples()
-        self.table_filler(table, f5, ["ID", "City"])
+        self.table_filler(table, tables[4], School._meta.sorted_field_names)
 
     def table_filler(self, table, parent, header):
-        for column in range(len(header)):
-            Label(parent, text=header[column]).grid(column=column, row=0)
-        for row in range(len(table)):
-            for column in range(len(table[row])):
-                Label(parent, text=table[row][column], borderwidth=2, background="yellow").grid(
-                    column=column, row=row + 1, sticky="e", padx=5, pady=2
-                )
 
-# rows = []
-# for i in range(5):
-#     cols = []
-#     for j in range(4):
-#         e = Entry(relief=RIDGE)
-#         e.grid(row=i, column=j, sticky=NSEW)
-#         e.insert(END, '%d.%d' % (i, j))
-#         cols.append(e)
-#     rows.append(cols)
-#
-# def onPress():
-#     for row in rows:
-#         for col in row:
-#             print(col.get()),
-#         print
-#
-# Button(text='Fetch', command=onPress).grid()
-# mainloop()
-#
-#     def table_filler(self, table, parent, header):
-#         for column in range(len(header)):
-#             Label(parent, text=header[column]).grid(column=column, row=0)
-#         for row in range(len(table)):
-#             for column in range(len(table[row])):
-#                 Label(parent, text=table[row][column], borderwidth=2, background="yellow").grid(
-#                     column=column, row=row + 1, sticky="e", padx=5, pady=2
-#                 )
+        maximum_lengths = [max(len(str(value)) for value in column) for column in zip(*table)]
+        maximum_lengths = [value if value > 6 else 6 for value in maximum_lengths]
+        self.rows = []
+        for column in range(len(table[0])):
+            e = Entry(parent, width=maximum_lengths[column]+2)
+            e.insert(END, header[column])
+            e.grid(column=column, row=0)
+
+        for row in range(len(table)):
+            self.cols = []
+            for column in range(len(table[row])):
+                e = Entry(parent, width=maximum_lengths[column]+2)
+                e.grid(column=column, row=row+1)
+                e.insert(END, str(table[row][column]))
+                self.cols.append(e)
+            self.rows.append(self.cols)
+        Button(parent, text='Fetch', command=self.onPress).grid()
+
+    def onPress(self):
+        for row in self.rows:
+            for col in row:
+                print(col.get()),
+            print
